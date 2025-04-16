a7b797694caeb8e3040d1c77adb9a8f42f7ce2d3
Vish501 <76504332+Vish501@users.noreply.github.com>
2025-04-16 14:59:17 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "stock-research-assistant",
+    version = "0.0.1",
+    author = "Vish501",
+    packages = find_packages(),
+    install_requires = []
+)
